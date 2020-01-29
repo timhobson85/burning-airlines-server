57a85f4fe145e3ae4ff9a520e05b37c1b99ea6d6
Kai Yang <kaiyang.ky2@gmail.com>
2020-01-29 15:50:55 +1100

    created associations between models

---
+++ db/migrate/20200129045010_add_flight_id_user_id_to_reservations.rb
@@ -0,0 +1,6 @@
+class AddFlightIdUserIdToReservations < ActiveRecord::Migration[5.2]
+  def change
+    add_column :reservations, :flight_id, :integer
+    add_column :reservations, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_035928) do
+ActiveRecord::Schema.define(version: 2020_01_29_045010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,9 +23,21 @@ ActiveRecord::Schema.define(version: 2020_01_29_035928) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.string "origin"
+    t.string "destination"
+    t.string "date"
+    t.string "plane"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "airplane_id"
+  end
+
   create_table "reservations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "flight_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
