87e1b7b6fd52ad3e972f83644bf524541ed4c0e1
Tim Hobson <timhobson85@gmail.com>
2020-01-30 14:04:51 +1100

    forms

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -70,6 +70,6 @@ class ReservationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def reservation_params
-      params.require(:reservation).permit(:flight_id, :user_id)
+      parsnsnams.require(:reservation).permit(:flight_id, :user_id)
     end
 end
